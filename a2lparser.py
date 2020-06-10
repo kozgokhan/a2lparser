79d45eecaa344c71d9c3ec8aca904304397b708d
Kaan Özgökhan <kozgokhan@yahoo.com>
2020-06-10 20:36:07 +0300

    commit

---
+++ a2lparser.py
@@ -0,0 +1,33 @@
+class CHARACTERISTIC:
+    type = ''; # VALUE, ASCII, VAL_BLK, CURVE, MAP, CUBOID, CUBE_4, CUBE_5
+    address = '';
+    record_layout = '';
+    compu_method = '';
+    upper_limit = 0;
+    lower_limit = 0;
+    annotation = '';
+    axis_descr = '';
+    bit_mask = '';
+    byte_order = '';
+    calibration_access = '';
+    comparison_quantity = '';
+
+
+
+class COMPU_METHOD:
+    conversion_method = '';
+
+class MEASUREMENT:
+    address = '';
+    byte_order = '';
+    compu_method = '';
+    upper_limit = 0;
+    lower_limit = 0;
+    bit_mask = '';
+
+
+class RECORD_LAYOUT:
+    alignment = '';
+
+class UNIT:
+    unit = '';
